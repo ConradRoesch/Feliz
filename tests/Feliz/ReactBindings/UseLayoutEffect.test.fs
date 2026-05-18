4f7b64c01c23f0e95022ce8e61a382b968b5283d
Kevin F <Freymaurer@gmx.de>
2026-05-18 15:03:13 +0200

    add tests for cleanup delegates #714

--- tests/Feliz/ReactBindings/UseLayoutEffect.test.fs
+++ tests/Feliz/ReactBindings/UseLayoutEffect.test.fs
@@ -7,144 +7,206 @@ open Feliz
 open Browser
 open Vitest
 
-type Components =
-    [<ReactComponent>]
-    static member ComponentUseLayoutEffectWithUnmount(effect: unit -> unit, disposeEffect: unit -> unit) =
-        React.useLayoutEffect(
-            (fun () ->
-                effect()
-                fun () -> disposeEffect()
+module Components =
+
+    type UseLayoutEffect =
+        [<ReactComponent>]
+        static member EffectWithUnmount(effect: unit -> unit, disposeEffect: unit -> unit) =
+            React.useLayoutEffect (
+                (fun () ->
+                    effect ()
+                    fun () -> disposeEffect ()
+                )
             )
-        )
 
-        Html.div [
+            Html.div [
 
-        ]
+            ]
 
-    [<ReactComponent>]
-    static member ComponentUseLayoutEffectIDisposable(effect: unit -> unit, disposeEffect: unit -> unit) =
-        React.useLayoutEffect(
-            (fun () ->
-                effect()
-                FsReact.createDisposable(disposeEffect)
+        [<ReactComponent>]
+        static member EffectWithIDisposable(effect: unit -> unit, disposeEffect: unit -> unit) =
+            React.useLayoutEffect (
+                (fun () ->
+                    effect ()
+                    FsReact.createDisposable (disposeEffect)
+                )
             )
-        )
 
-        Html.div [
+            Html.div [
 
-        ]
+            ]
 
-    [<ReactComponent>]
-    static member ComponentUseLayoutEffectOnceWithUnmount(effect: unit -> unit, disposeEffect: unit -> unit) =
-        let setup: unit -> (unit -> unit) =
-            fun () ->
-                effect()
-                fun () -> disposeEffect()
+        /// #714
+        [<ReactComponent>]
+        static member OnlyCleanup(disposeEffect: unit -> unit) =
+            React.useLayoutEffect ((fun () -> fun () -> disposeEffect ()), [||])
 
-        React.useLayoutEffectOnce(setup)
+            Html.div [
 
-        Html.div [
+            ]
 
-        ]
+    type UseLayoutEffectOnce =
 
-    [<ReactComponent>]
-    static member ComponentUseLayoutEffectOnceIDisposable(effect: unit -> unit, disposeEffect: unit -> unit) =
-        React.useLayoutEffectOnce(
-            (fun () ->
-                effect()
-                FsReact.createDisposable(disposeEffect)
+        [<ReactComponent>]
+        static member EffectWithUnmount(effect: unit -> unit, disposeEffect: unit -> unit) =
+            let setup: unit -> (unit -> unit) =
+                fun () ->
+                    effect ()
+                    fun () -> disposeEffect ()
+
+            React.useLayoutEffectOnce (setup)
+
+            Html.div [
+
+            ]
+
+        [<ReactComponent>]
+        static member EffectWithIDisposable(effect: unit -> unit, disposeEffect: unit -> unit) =
+            React.useLayoutEffectOnce (
+                (fun () ->
+                    effect ()
+                    FsReact.createDisposable (disposeEffect)
+                )
             )
-        )
 
-        Html.div [
+            Html.div [
 
-        ]
+            ]
 
-describe "useLayoutEffect" <| fun _ ->
-    testPromise "calls effect on mount and disposeEffect on unmount" <| fun _ -> promise {
+        // #714
+        [<ReactComponent>]
+        static member OnlyCleanup(disposeEffect: unit -> unit) =
+            React.useLayoutEffectOnce ((fun () -> fun () -> disposeEffect ()))
 
-        let effect: unit -> unit = vi.fn(fun () -> ())
-        let dispose: unit -> unit = vi.fn(fun () -> ())
+            Html.div [
+
+            ]
+
+describe "useLayoutEffect"
+<| fun _ ->
+    testPromise "calls effect on mount and disposeEffect on unmount"
+    <| fun _ -> promise {
+
+        let effect: unit -> unit = vi.fn (fun () -> ())
+        let dispose: unit -> unit = vi.fn (fun () -> ())
 
         // Render the component
-        let renderResult = RTL.render (
-            Components.ComponentUseLayoutEffectWithUnmount(effect, dispose)
-        )
+        let renderResult =
+            RTL.render (Components.UseLayoutEffect.EffectWithUnmount(effect, dispose))
 
         // Check that effect was called once on mount
         expect(effect).toHaveBeenCalledTimes 1 //"Effect should be called once on mount"
-        expect(dispose).toHaveBeenCalledTimes 0 
+        expect(dispose).toHaveBeenCalledTimes 0
 
         // Unmount the component
-        renderResult.unmount()
+        renderResult.unmount ()
 
         // Check that disposeEffect was called once on unmount
         expect(effect).toHaveBeenCalledTimes 1
         expect(dispose).toHaveBeenCalledTimes 1
     }
 
-    testPromise "calls effect on mount and IDisposable.Dispose() on unmount" <| fun _ -> promise {
+    testPromise "calls effect on mount and IDisposable.Dispose() on unmount"
+    <| fun _ -> promise {
 
-        let effect: unit -> unit = vi.fn(fun () -> ())
-        let dispose: unit -> unit = vi.fn(fun () -> ())
+        let effect: unit -> unit = vi.fn (fun () -> ())
+        let dispose: unit -> unit = vi.fn (fun () -> ())
 
         // Render the component
-        let renderResult = RTL.render (
-            Components.ComponentUseLayoutEffectIDisposable(effect, dispose)
-        )
+        let renderResult =
+            RTL.render (Components.UseLayoutEffect.EffectWithIDisposable(effect, dispose))
 
         // Check that effect was called once on mount
         expect(effect).toHaveBeenCalledTimes 1 //"Effect should be called once on mount"
-        expect(dispose).toHaveBeenCalledTimes 0 
+        expect(dispose).toHaveBeenCalledTimes 0
 
         // Unmount the component
-        renderResult.unmount()
+        renderResult.unmount ()
 
         // Check that disposeEffect was called once on unmount
         expect(effect).toHaveBeenCalledTimes 1
         expect(dispose).toHaveBeenCalledTimes 1
     }
 
-    testPromise "useLayoutEffectOnce cleanup function runs on unmount" <| fun _ -> promise {
+    testPromise "calls cleanup function on unmount with no function body #714"
+    <| fun _ -> promise {
 
-        let effect: unit -> unit = vi.fn(fun () -> ())
-        let dispose: unit -> unit = vi.fn(fun () -> ())
+        let dispose: unit -> unit = vi.fn (fun () -> ())
 
         // Render the component
-        let renderResult = RTL.render (
-            Components.ComponentUseLayoutEffectOnceWithUnmount(effect, dispose)
-        )
+        let renderResult = RTL.render (Components.UseLayoutEffect.OnlyCleanup(dispose))
+
+        // Check that effect was called once on mount
+        expect(dispose).toHaveBeenCalledTimes 0
+
+        // Unmount the component
+        renderResult.unmount ()
+
+        // Check that disposeEffect was called once on unmount
+        expect(dispose).toHaveBeenCalledTimes 1
+    }
+
+
+describe "useLayoutEffectOnce"
+<| fun _ ->
+    testPromise "cleanup function runs on unmount"
+    <| fun _ -> promise {
+
+        let effect: unit -> unit = vi.fn (fun () -> ())
+        let dispose: unit -> unit = vi.fn (fun () -> ())
+
+        // Render the component
+        let renderResult =
+            RTL.render (Components.UseLayoutEffectOnce.EffectWithUnmount(effect, dispose))
 
         // Check that effect was called once on mount
         expect(effect).toHaveBeenCalledTimes 1
         expect(dispose).toHaveBeenCalledTimes 0
 
         // Unmount the component
-        renderResult.unmount()
+        renderResult.unmount ()
 
         // Check that cleanup was called once on unmount
         expect(effect).toHaveBeenCalledTimes 1
         expect(dispose).toHaveBeenCalledTimes 1
     }
 
-    testPromise "useLayoutEffectOnce IDisposable.Dispose() runs on unmount" <| fun _ -> promise {
+    testPromise "IDisposable.Dispose() runs on unmount"
+    <| fun _ -> promise {
 
-        let effect: unit -> unit = vi.fn(fun () -> ())
-        let dispose: unit -> unit = vi.fn(fun () -> ())
+        let effect: unit -> unit = vi.fn (fun () -> ())
+        let dispose: unit -> unit = vi.fn (fun () -> ())
 
         // Render the component
-        let renderResult = RTL.render (
-            Components.ComponentUseLayoutEffectOnceIDisposable(effect, dispose)
-        )
+        let renderResult =
+            RTL.render (Components.UseLayoutEffectOnce.EffectWithIDisposable(effect, dispose))
 
         // Check that effect was called once on mount
         expect(effect).toHaveBeenCalledTimes 1
         expect(dispose).toHaveBeenCalledTimes 0
 
         // Unmount the component
-        renderResult.unmount()
+        renderResult.unmount ()
 
         // Check that disposeEffect was called once on unmount
         expect(effect).toHaveBeenCalledTimes 1
+        expect(dispose).toHaveBeenCalledTimes 1
+    }
+
+    testPromise "calls cleanup function on unmount with no function body #714"
+    <| fun _ -> promise {
+
+        let dispose: unit -> unit = vi.fn (fun () -> ())
+
+        // Render the component
+        let renderResult = RTL.render (Components.UseLayoutEffectOnce.OnlyCleanup(dispose))
+
+        // Check that effect was called once on mount
+        expect(dispose).toHaveBeenCalledTimes 0
+
+        // Unmount the component
+        renderResult.unmount ()
+
+        // Check that disposeEffect was called once on unmount
         expect(dispose).toHaveBeenCalledTimes 1
     }
